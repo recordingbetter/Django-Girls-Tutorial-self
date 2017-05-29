f9984b2f43feedf304f9f80367fd9cd5fd3d433d
Joe Donghee Cho <recordingbetter@gmail.com>
2017-05-29 19:25:13 +0900

    template setting, add temporary post_list view

--- django_site/blog/views.py
+++ django_site/blog/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+
+def post_list(request):
+    return HttpResponse('<html><body>Post List</body></html>')
