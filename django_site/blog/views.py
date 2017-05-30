1b6157f19a2f808dcb0aaaedb99a548592a620ec
Joe Donghee Cho <recordingbetter@gmail.com>
2017-05-30 19:14:39 +0900

    add post_detail

--- django_site/blog/views.py
+++ django_site/blog/views.py
@@ -4,8 +4,16 @@ from .models import Post
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now())
+    posts = Post.objects.all()
     context = {
         'posts': posts,
     }
     return render(request, 'blog/post_list.html', context = context)
+
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_detail.html', context)
--- django_site/djangosite/urls.py
+++ django_site/djangosite/urls.py
@@ -19,5 +19,6 @@ from blog import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.post_list),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name = 'post_detail')
 ]
